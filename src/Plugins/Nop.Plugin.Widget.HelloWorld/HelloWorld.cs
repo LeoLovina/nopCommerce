da75668d374048b62e7e9f3435b387a01d0c0490
Leo Chen <lchen@equuscs.com>
2022-08-29 16:10:08 +0800

    add new plugin

---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Widget.HelloWorld
+{
+    public class HelloWorld : BasePlugin
+    {
+        public override async Task InstallAsync()
+        {
+            // logic during installation goes here ..
+            await base.InstallAsync();
+        }
+
+        public override async Task UninstallAsync()
+        {
+            await base.UninstallAsync();
+        }
+    }
+}
