faae75d7b2046f0d02c5e5f6debbb1e7b358d251
Leo Chen <lchen@equuscs.com>
2022-09-04 08:51:40 +0800

    hello plugin

---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/Components/HelloWorldViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widget.HelloWorld.Components
+{
+    [ViewComponent(Name = "HelloWorldWidget")]
+    public class HelloWorldViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke(string widgetZone, object additionalData)
+        {
+            return Content("Hello World");
+        }
+    }
+}
--- src/Plugins/Nop.Plugin.Widget.HelloWorld/HelloWorld.cs
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/HelloWorld.cs
@@ -1,10 +1,39 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nop.Services.Cms;
 using Nop.Services.Plugins;
 
 namespace Nop.Plugin.Widget.HelloWorld
 {
-    public class HelloWorld : BasePlugin
+    public class HelloWorld : BasePlugin, IWidgetPlugin
     {
+        /// <summary>
+        /// Gets a value indicating whether to hide this plugin on the widget list page in the admin area
+        /// </summary>
+        public bool HideInWidgetList => false;
+
+        /// <summary>
+        /// Gets a name of a view component for displaying widget
+        /// </summary>
+        /// <param name="widgetZone">Name of the widget zone</param>
+        /// <returns>View component name</returns>
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            return "HelloWorldWidget";
+        }
+
+        /// <summary>
+        /// Gets widget zones where this widget should be rendered
+        /// </summary>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the widget zones
+        /// </returns>
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            return Task.FromResult<IList<string>>(new List<string> { "home_page_before_categories" });
+        }
+
         public override async Task InstallAsync()
         {
             // logic during installation goes here ..
